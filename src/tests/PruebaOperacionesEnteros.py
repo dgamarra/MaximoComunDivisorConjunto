5bbe14f3a08faa05d1fcd17d8989c7450adbbd69
dgamarra <dgamarra@continental.edu.pe>
2023-02-13 07:27:09 -0500

    Implementación MCD de n números enteros

---
+++ src/tests/PruebaOperacionesEnteros.py
@@ -0,0 +1,67 @@
+import unittest
+from src.logica.OperacionesEnteros import OperacionesEnteros
+from src.logica.OperacionesEnteros import FaltanParametros
+
+class PruebaOperacionesEnteros(unittest.TestCase):
+    def test_MCD_dosNumerosPositivos_retornaMCD(self):
+        # Arrange
+        numero1 = 18
+        numero2 = 24
+        resultadoEsperado = 6
+        operacion = OperacionesEnteros([numero1, numero2])
+
+        # Do
+        resultadoActual = operacion.calcularMCD()
+
+        # Assert
+        self.assertEqual(resultadoEsperado, resultadoActual)
+
+    def test_MCD_tresNumerosPositivos_retornaMCD(self):
+        # Arrange
+        numero1 = 18
+        numero2 = 24
+        numero3 = 30
+        resultadoEsperado = 6
+        operacion = OperacionesEnteros([numero1, numero2, numero3])
+
+        # Do
+        resultadoActual = operacion.calcularMCD()
+
+        # Assert
+        self.assertEqual(resultadoEsperado, resultadoActual)
+
+    def test_MCD_cuatroNumerosPositivos_retornaMCD(self):
+        # Arrange
+        numero1 = 18
+        numero2 = 24
+        numero3 = 30
+        numero4 = 4
+        resultadoEsperado = 2
+        operacion = OperacionesEnteros([numero1, numero2, numero3, numero4])
+
+        # Do
+        resultadoActual = operacion.calcularMCD()
+
+        # Assert
+        self.assertEqual(resultadoEsperado, resultadoActual)
+
+    def test_MCD_unNumeroPositivo_lanzaExcepcion(self):
+        # Arrange
+        numero1 = 18
+        operacion = OperacionesEnteros([numero1])
+
+        # Assert
+        self.assertTrue(True)
+        with self.assertRaises(FaltanParametros):
+            operacion.calcularMCD()
+
+    def test_MCD_unaCadena_lanzaExcepcion(self):
+        # Arrange
+        numero1 = "18a"
+        numero2 = 13
+        operacion = OperacionesEnteros([numero1, numero2])
+
+        # Assert
+        self.assertTrue(True)
+        with self.assertRaises(ValueError):
+            operacion.calcularMCD()
